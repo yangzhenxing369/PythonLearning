acafdd14dbcdccdd64fa9f612322177e5dfaaf2a
yangzhenxing <yangzhenxing369@sina.com>
2013-08-04 23:33:12 +0800

    init

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+print "hello world"
+
+print "hello world"
+
+
+for x in ['x', 'a', 'b', 'c']:
+ 	print x
+
+
+for x in xrange(1,10):
+    print "china" 	
+
+
---
+++ mytest.py
@@ -0,0 +1,21 @@
+# if else 
+
+"""
+a = 1
+b = 2
+
+if a > b :
+	print a 
+else:
+	print b 
+
+
+c = id(a)
+print c
+
+d = type(c)
+
+print d 
+"""
+
+sample_list = ["a", 1, ("a")]
